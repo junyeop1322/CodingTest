55bae8ac2434133044c6508c146b79fb576db30b
YEOP <88226521+junyeop1322@users.noreply.github.com>
2023-07-20 22:15:55 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+        
+        double ans = 0;
+
+        ans = A / B;
+
+        System.out.println(ans);
+    }
+}
