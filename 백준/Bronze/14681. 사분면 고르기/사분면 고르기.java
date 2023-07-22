60a530a8b147d285e6cd6be22638df88e697b166
YEOP <88226521+junyeop1322@users.noreply.github.com>
2023-07-22 16:59:23 +0900

    [Bronze V] Title: 사분면 고르기, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int ans = 0;
+		
+		if (x > 0) {
+			if (y > 0) {
+				ans = 1;
+			}
+			else {
+				ans = 4;
+			}
+		}
+		else {
+			if (y > 0) {
+				ans = 2;
+			}
+			else {
+				ans = 3;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+
+}
