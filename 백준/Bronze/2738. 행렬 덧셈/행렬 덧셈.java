00dadae34499b423b9f3cc92606f07f4526404b0
YEOP <88226521+junyeop1322@users.noreply.github.com>
2023-07-29 22:55:06 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 660 ms, Memory: 37036 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+        int[][] brr = new int[n][m];
+        int[][] crr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                brr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                crr[i][j] = arr[i][j] + brr[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(crr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
