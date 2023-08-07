2ab19e194830bdd418b94818a55957b58e61dfd9
YEOP <88226521+junyeop1322@users.noreply.github.com>
2023-08-07 21:44:31 +0900

    [Silver III] Title: N과 M (4), Time: 772 ms, Memory: 23904 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+
+	static int N;
+	static int M;
+	static int[] res;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		res = new int[M];
+		
+		comb(0, 1);
+	}
+	
+	static void comb(int cnt, int start) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				System.out.print(res[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = start; i <= N; i++) {
+			res[cnt] = i;
+			
+			comb(cnt+1, i);
+		}
+	}
+
+}
