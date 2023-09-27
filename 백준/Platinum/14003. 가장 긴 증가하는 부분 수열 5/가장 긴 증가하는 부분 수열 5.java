12ee7184996573fd8196fad929248bb93f25114a
YEOP <88226521+junyeop1322@users.noreply.github.com>
2023-09-27 15:51:03 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 980 ms, Memory: 209284 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int[] memo;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		memo = new int[n+1];
+		int[] ans = new int[n+1];
+		Arrays.fill(memo, Integer.MIN_VALUE);
+
+		int len = 0;
+		int idx = 0;
+		
+		for (int i = 0; i < n; i++) {
+			if (arr[i] > memo[len]) {
+				len++;
+				memo[len] = arr[i];
+				ans[i+1] = len;
+			} else {
+				idx = binarySearch(0, len, arr[i]);
+				memo[idx] = arr[i];
+				ans[i+1] = idx;
+			}
+		}
+		
+		sb.append(len).append("\n");
+//		System.out.println(Arrays.toString(ans));
+		Stack<Integer> stack = new Stack<Integer>();
+		int num = len;
+		for (int i = n; i > 0; i--) {
+			if (ans[i] == num) {
+				stack.push(arr[i-1]);
+				num--;
+			}
+		}
+		
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop() + " ");
+		}
+		System.out.println(sb);
+		
+	}
+	
+	static int binarySearch(int left, int right, int val) {
+		int mid = 0;
+		
+		while(left < right) {
+			mid = (left+right) / 2;
+			
+			if (memo[mid] < val) {
+				left = mid+1;
+			} else {
+				right = mid;
+			}
+			
+		}
+		
+		return right;
+	}
+
+}
