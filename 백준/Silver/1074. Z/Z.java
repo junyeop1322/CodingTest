8f97d4cfd0b01f83b229429ae8b3798987e6284c
YEOP <88226521+junyeop1322@users.noreply.github.com>
2023-10-11 14:57:55 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int ans;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		int size = (int) Math.pow(2, N);
+		ans = 0;
+		
+		find(size, r, c);
+		System.out.println(ans);
+	}
+	
+	static void find(int size, int r, int c) {
+		if (size == 1) {
+			return;
+		}
+		
+		if (r >= size/2 && c >= size/2) {
+			ans += (size*size / 4) * 3;
+			find(size/2, r-size/2, c-size/2);
+		} else if (r >= size/2 && c < size/2) {
+			ans += (size*size /4) * 2;
+			find(size/2, r-size/2, c);
+			
+		} else if (r < size/2 && c >= size/2) {
+			ans += (size*size / 4);
+			find(size/2, r, c-size/2);
+			
+		} else if (r < size/2 && c < size/2) {
+			find(size/2, r, c);
+		}
+		
+	}
+	
+
+}
