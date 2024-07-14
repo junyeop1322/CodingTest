fae058ed6ae35e3bca2853aab29749088ff80559
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-14 11:16:01 +0900

    [level 1] Title: 옹알이 (2), Time: 1.38 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        
+        for (int i = 0; i < babbling.length; i++) {
+            String str = babbling[i];
+            if (str.contains("ayaaya") || str.contains("yeye") || str.contains("woowoo") || str.contains("mama")) {
+                continue;
+            }
+            
+            str = str.replace("aya", " ");
+            str = str.replace("ye", " ");
+            str = str.replace("woo", " ");
+            str = str.replace("ma", " ");
+            str = str.replace(" ", "");
+            
+            if (str.length() == 0) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
