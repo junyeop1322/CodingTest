a280df2151efd54d53960dd8587d5efa20eba6f4
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-10-15 11:23:30 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 24.09 ms, Memory: 68.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        String[] str = s.split(" ");
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < str.length; i++) {
+            int num = Integer.parseInt(str[i]);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        
+        answer += Integer.toString(min) + " " + Integer.toString(max);
+        
+        return answer;
+    }
+}
