1afb44e34b52235f93f077563f8b851e8d52b13f
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-10-18 18:13:00 +0900

    [level 2] Title: 주식가격, Time: 81.37 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        Stack<Integer> stk = new Stack<>();
+        
+        for (int i = 0; i < prices.length; i++) {
+            while(!stk.isEmpty() && prices[i] < prices[stk.peek()]) {
+                answer[stk.peek()] = i - stk.peek();
+                stk.pop();
+            }
+            stk.push(i);
+        }
+        
+        while(!stk.isEmpty()) {
+            answer[stk.peek()] = prices.length - stk.peek() - 1;
+            stk.pop();
+        }
+        
+        return answer;
+    }
+}
