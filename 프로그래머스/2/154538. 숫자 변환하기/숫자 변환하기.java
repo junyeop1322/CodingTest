e7b694078e021b6c1a66ddbf2d6398d69be295bb
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-12-08 17:32:44 +0900

    [level 2] Title: 숫자 변환하기, Time: 33.53 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int x, int y, int n) {
+        int[] arr = new int[y+1];
+        Arrays.fill(arr, -1);
+        
+        arr[x] = 0;
+        
+        for (int i = x; i <= y; i++) {
+            if (arr[i] == -1) {
+                continue;
+            }
+            
+            if (i+n <= y) {
+                if (arr[i+n] == -1) {
+                    arr[i+n] = arr[i]+1;
+                } else {
+                    arr[i+n] = Math.min(arr[i+n], arr[i]+1);
+                }
+            }
+            if (i*2 <= y) {
+                if (arr[i*2] == -1) {
+                    arr[i*2] = arr[i]+1;
+                } else {
+                    arr[i*2] = Math.min(arr[i*2], arr[i]+1);
+                }
+            }
+            if (i*3 <= y) {
+                if (arr[i*3] == -1) {
+                    arr[i*3] = arr[i]+1;
+                } else {
+                    arr[i*3] = Math.min(arr[i*3], arr[i]+1);
+                }
+            }
+        }
+        
+        return arr[y];
+    }
+}
