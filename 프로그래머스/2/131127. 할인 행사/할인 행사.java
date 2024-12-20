03b13f7cfb9c4e1c4b5124bf635c86c4e34fed60
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-12-20 13:51:02 +0900

    [level 2] Title: 할인 행사, Time: 47.31 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < want.length; i++) {
+            map.put(want[i],number[i]);
+        }
+        
+        for(int i = 0; i < 10; i++) {
+            if(map.containsKey(discount[i])) {
+                map.put(discount[i], map.get(discount[i]) - 1);
+            }
+        }
+        
+        if(isMatch(map)) {
+            answer++;
+        }
+        
+        for(int i = 10; i<discount.length; i++) {
+            if(map.containsKey(discount[i - 10])) {
+                map.put(discount[i - 10], map.get(discount[i - 10]) + 1);
+            }
+            if(map.containsKey(discount[i])) {
+                map.put(discount[i], map.get(discount[i]) - 1);
+            }
+            
+            if(isMatch(map)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public boolean isMatch(HashMap<String, Integer> map) {
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
