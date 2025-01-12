f58a4c000bce90c7c1e856d7d181f64217d4094b
YEOP <88226521+junyeop1322@users.noreply.github.com>
2025-01-12 14:49:28 +0900

    [Gold III] Title: 모양 만들기, Time: 776 ms, Memory: 240244 KB -BaekjoonHub

--- 백준/Gold/16932. 모양 만들기/모양 만들기.java
+++ 백준/Gold/16932. 모양 만들기/모양 만들기.java
@@ -2,110 +2,114 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	
+	static int n, m;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] sumArr = new int[500002];
+	
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
 
-    public static class Shape {
-        int y;
-        int x;
-        public Shape(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
-    public static int n;
-    public static int m;
-    public static final int[][] dists = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int[][] map = new int[n][m];
-        for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		map = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		Queue<int[]> zeroQ = new LinkedList<>();
+		int cnt = 2;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] == 1) {
+					bfs(i, j, cnt);
+					cnt++;
+				} else if (map[i][j] == 0) {
+					zeroQ.add(new int[] {i, j});
+				}
+			}
+		}
+		
+		int ans = 0;
+		
+		while(!zeroQ.isEmpty()) {
+			int[] cur = zeroQ.poll();
+			int x = cur[0];
+			int y = cur[1];
+			
+			ans = Math.max(ans, func(x, y));
+		}
+		
+		System.out.println(ans);
+	}
+	
+	static int func(int x, int y) {
+		int sol = 1;
+		
+		HashSet<Integer> set = new HashSet<>();
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+				continue;
+			}
+			
+			int next = map[nx][ny];
+			
+			if (next > 1 && !set.contains(next)) {
+				sol += sumArr[next];
+				set.add(next);
+			}
+		}
+		
+		return sol;
+	}
+	
+	static void bfs(int x, int y, int cnt) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {x, y});
+		
+		int sum = 0;
+		
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			int sx = cur[0];
+			int sy = cur[1];
+			
+			if (map[sx][sy] != 1) {
+				continue;
+			}
+			
+			map[sx][sy] = cnt;
+			sum++;
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = sx + dx[i];
+				int ny = sy + dy[i];
+				
+				if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+					continue;
+				}
+				
+				if (map[nx][ny] == 1) {
+					q.add(new int[] {nx, ny});
+				}
+			}
+		}
+		
+		sumArr[cnt] = sum;
+		
+	}
 
-        int groupN = 2;
-        int[] sumOfGroup = new int[500002];
-        Queue<Shape> zeroQ = new LinkedList<>();
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(map[i][j] == 1) {
-                    makeGroup(map, new Shape(i, j), groupN, sumOfGroup);
-                    groupN++;
-                } else if(map[i][j] == 0) {
-                	zeroQ.add(new Shape(i, j));
-                }
-            }
-        }
-        int ans = 0;
-        while(!zeroQ.isEmpty()) {
-            Shape curShape = zeroQ.poll();
-            ans = Math.max(ans, sumOfMerge(map, new Shape(curShape.y, curShape.x), sumOfGroup));
-        }
-        
-        System.out.println(ans);
-    }
-    
-    public static int sumOfMerge(int[][] map, Shape startShape, int[] sumOfGroup){
-        int i = startShape.y;
-        int j = startShape.x;
-
-        int solution = 1;
-
-        HashSet<Integer> set = new HashSet<>();
-        for(int[] dist : dists) {
-            int nextY = i+dist[0];
-            int nextX = j+dist[1];
-
-            if(nextY >= n || nextY < 0 || nextX >= m || nextX < 0) {
-            	continue;
-            }
-            
-            int curGroup = map[nextY][nextX];
-            if(curGroup > 1 && !set.contains(curGroup)) {
-                solution += sumOfGroup[curGroup];
-                set.add(curGroup);
-            }
-        }
-        
-        return solution;
-    }
-    
-    public static void makeGroup(int[][] map, Shape startShape, int groupN,
-                                 int[] sumOfGroup){
-        Queue<Shape> Q = new LinkedList<>();
-        Q.add(startShape);
-
-        int sum = 0;
-        while(!Q.isEmpty()) {
-            Shape curShape = Q.poll();
-            int curY = curShape.y;
-            int curX = curShape.x;
-            
-            if(map[curY][curX]!=1) {
-            	continue;
-            }
-
-            map[curY][curX] = groupN;
-            sum++;
-
-            for(int[] dist : dists) {
-                int nextY = curY+dist[0];
-                int nextX = curX+dist[1];
-
-                if(nextY >= n || nextY < 0 || nextX >= m || nextX < 0) {
-                	continue;
-                }
-                
-                if(map[nextY][nextX]==1) {
-                    Q.add(new Shape(nextY, nextX));
-                }
-            }
-        }
-        sumOfGroup[groupN] = sum;
-    }
 }
