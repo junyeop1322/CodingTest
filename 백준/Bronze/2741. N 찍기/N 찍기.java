bce57858fa1c2e4d65aaf68456442da4ad42ffdf
YEOP <88226521+junyeop1322@users.noreply.github.com>
2025-02-08 23:34:58 +0900

    [Bronze V] Title: N 찍기, Time: 756 ms, Memory: 30312 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int n = Integer.parseInt(st.nextToken());
+		
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+	}
+}
