6ed78e2a892930c3af1e3e81a9edf730786523d6
YEOP <88226521+junyeop1322@users.noreply.github.com>
2025-04-19 21:49:56 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Integer> h = new HashSet<Integer>();
+ 
+		
+		for (int i = 0; i < 10; i++) {
+			h.add(Integer.parseInt(br.readLine()) % 42);
+		}
+		
+		System.out.print(h.size());
+	}
+}
