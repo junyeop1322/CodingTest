b37070844742e0eb75a965380a636490d1f5aec8
YEOP <88226521+junyeop1322@users.noreply.github.com>
2025-06-20 19:02:42 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 124 ms, Memory: 14492 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i-j] + arr[j]);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
