154d98f238eeed14cc0dca0bad0395aab1052246
YEOP <88226521+junyeop1322@users.noreply.github.com>
2025-07-09 21:57:19 +0900

    [Bronze II] Title: 단어의 개수, Time: 136 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		int count = 0;
+		int pre_str = 32;
+		int str ;
+		
+		while(true) {
+			str = System.in.read();
+            
+			if(str == 32) {
+				if(pre_str != 32) count++;
+			}
+ 
+			else if(str == 10) {
+				if(pre_str != 32) count++;
+				break;
+			}
+			
+			pre_str = str;
+			
+		}
+		
+		System.out.println(count);
+	}
+ 
+}
