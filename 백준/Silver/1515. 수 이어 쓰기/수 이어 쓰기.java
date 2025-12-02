383922b1e36fd812758276ab3a75a39118a07551
YEOP <88226521+junyeop1322@users.noreply.github.com>
2025-12-02 19:22:53 +0900

    [Silver II] Title: 수 이어 쓰기, Time: 132 ms, Memory: 16264 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String number = br.readLine();
+
+    int pointer = 0;
+    String base = null;
+    loop :
+    for (int i = 1; i < 30000; i++) {
+      base = String.valueOf(i);
+      for (int j = 0; j < base.length(); j++) {
+        if (number.charAt(pointer) == base.charAt(j)) {
+          pointer++;
+        }
+        if (pointer == number.length()) {
+          break loop;
+        }
+      }
+    }
+
+    System.out.println(base);
+  }
+}
